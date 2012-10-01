82074ed9ebbede42aa493804b61c778af596821c
Kocsis Olivér <okocsis@okocsis-MBA.local>
2012-10-02 00:36:53 +0200

    Initial Commit

---
+++ Data/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Data
+//
+//  Created by Kocsis Olivér on 2012.10.02..
+//  Copyright (c) 2012 Kocsis Olivér. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
