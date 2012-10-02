3d4e0f30b3a6c57849631599b9c1954daf6ff253
Kocsis Olivér <okocsis@okocsis-MBA.local>
2012-10-02 15:12:21 +0200

    kifejtes

--- Data/DataClass.cpp
+++ Data/DataClass.cpp
@@ -1,118 +1,27 @@
-//#include <iostream>
-//#include <fstream>
-//#include "DynamicStack.h"
-//
-//
-//using namespace std;
-//
-//
-//
-//Node::Node()
-//{
-//    prev = NULL;
-//    data = "";
-//}
-//
-//Node::Node(string inVal)
-//{
-//    prev = NULL;
-//    
-//    data = inVal;
-//}
-//
-//DynamicStack::DynamicStack()
-//{
-//    head = new Node;
-//}
-//
-//void DynamicStack::print()
-//{
-//    Node* tempHead = this->head;
-//    
-//    while (tempHead != NULL)
-//    {
-//        cout<<tempHead->data<< "\n";
-//        tempHead = tempHead->prev;
-//    }
-//}
-//
-//bool DynamicStack::isEmpty()
-//{
-//    return !(head->prev);
-//}
-//
-//string DynamicStack::top()
-//{
-//    if (!isEmpty())
-//    {
-//        return head->data;
-//    }
-//    else
-//    {
-//        throw (string)"Stack is empty";
-//    }
-//    
-//    return "";
-//}
-//
-//string DynamicStack::pop()
-//{
-//    string temp = "";
-//    Node* tempHead(NULL);
-//    if (!isEmpty())
-//    {
-//        tempHead = head;
-//        temp = head->data;
-//        head = head->prev;
-//        delete  tempHead;
-//    }
-//    else
-//    {
-//        throw (string)"Stack is empty";
-//    }
-//    return temp;
-//}
-//
-//void DynamicStack::push(string inVal)
-//{
-//    Node* temp = new Node (inVal);
-//    temp->prev = head;
-//    head = temp;
-//    head->data = inVal;
-//}
-
 
 
 #include <iostream>
+#include "DataClass.h"
 using namespace std;
-
-
-template <typename DataTemplate>
-class DataClass
-{
-public:
-    class Node {
-    public:
-        DataTemplate data;
-        Node* prevNode;
-        Node() :  prevNode(NULL)
-        {
-        }
-        Node(Node* inNode,DataTemplate inData) : data(inData), prevNode(inNode)
-        {
-        }
-    };
-    Node* head;
     
-    DinamicStack()
+    DinamicStack::DinamicStack()
     {
         head = NULL;
     }
-    bool isEmpty()
+    DinamicStack::~DinamicStack()
+    {
+        while(head!=0)
+        {
+            Node *n=head;
+            head=head->prevNode;
+            delete n;
+        }
+    }
+    bool DinamicStack::isEmpty()
     {
         return !(bool)head;
     }
-    void print(string &outDataTemplate)
+    void DinamicStack::print(string &outDataTemplate)
     {
         //                Node* tempPtr;
         //                tempPtr = this->head;
@@ -124,7 +33,7 @@ public:
         //                    tempPtr = tempPtr->prevNode;
         //                }
     }
-    DataTemplate top()
+    DataTemplate DinamicStack::top()
     {
         if (!isEmpty())
         {
@@ -136,7 +45,7 @@ public:
         }
         return 0;
     }
-    DataTemplate pop()
+    DataTemplate DinamicStack::pop()
     {
         if (isEmpty()) {
             throw (string)"nemjo";
@@ -148,22 +57,14 @@ public:
         
         return tempData;
     }
-    void push(DataTemplate value)
+    void DinamicStack::push(DataTemplate value)
     {
         Node* tempNode = new Node;
         tempNode->data = value;
         tempNode->prevNode = head;
         head = tempNode;
     }
-    ~DinamicStack()
-    {
-        while(head!=0)
-        {
-            Node *n=head;
-            head=head->prevNode;
-            delete n;
-        }
-    }
+
     
 };
 
