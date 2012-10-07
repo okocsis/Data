7de9429eccf2a36d217f86516972cc16339489a5
okocsis <okocsis88@gmail.com>
2012-10-08 01:36:52 +0200

    text format

--- Data/DataClass.cpp
+++ Data/DataClass.cpp
@@ -1,98 +1,110 @@
 
-
 #include <iostream>
 #include "DataClass.h"
 
-
 using namespace std;
 
+template<typename DataTemplate>
+DataClass<DataTemplate>::DataClass() :
+		head(NULL)
+{
+}
+template<typename DataTemplate>
+DataClass<DataTemplate>::~DataClass()
+{
+	while (head != 0)
+	{
+		Node *n = head;
+		head = head->prevNode;
+		delete n;
+	}
+}
+template<typename DataTemplate>
+bool DataClass<DataTemplate>::isEmpty()
+{
+	return !(bool) head;
+}
+template<typename DataTemplate>
+void DataClass<DataTemplate>::print(string &outDataTemplate)
+{
+	//                Node* tempPtr;
+	//                tempPtr = this->head;
+	//        stringstream stream;
+	//                while (tempPtr != NULL)
+	//                {
+	//
+	//                    outDataTemplate. (tempPtr->data + "\n");
+	//                    tempPtr = tempPtr->prevNode;
+	//                }
+}
+template<typename DataTemplate>
+DataTemplate DataClass<DataTemplate>::top()
+{
+	if (!isEmpty())
+	{
+		return head->data;
+	}
+	else
+	{
+		throw(string) "nemjo";
+	}
+	return 0;
+}
+template<typename DataTemplate>
+DataTemplate DataClass<DataTemplate>::pop()
+{
+	if (isEmpty())
+	{
+		throw(string) "nemjo";
+	}
+	DataTemplate tempData = head->data;
+	Node* tempHead = head;
+	head = head->prevNode;
+	delete tempHead;
 
+	return tempData;
+}
+template<typename DataTemplate>
+void DataClass<DataTemplate>::push(DataTemplate value)
+{
+	Node* tempNode = new Node;
+	tempNode->data = value;
+	tempNode->prevNode = head;
+	head = tempNode;
+}
 
-    template <typename DataTemplate>
-    DataClass<DataTemplate>::DataClass() : head(NULL)
-    {}
-    template <typename DataTemplate>
-    DataClass<DataTemplate>::~DataClass()
-    {
-        while(head!=0)
-        {
-            Node *n=head;
-            head=head->prevNode;
-            delete n;
-        }
-    }
-    template <typename DataTemplate>
-    bool DataClass<DataTemplate>::isEmpty()
-    {
-        return !(bool)head;
-    }
-    template <typename DataTemplate>
-    void DataClass<DataTemplate>::print(string &outDataTemplate)
-    {
-        //                Node* tempPtr;
-        //                tempPtr = this->head;
-        //        stringstream stream;
-        //                while (tempPtr != NULL)
-        //                {
-        //
-        //                    outDataTemplate. (tempPtr->data + "\n");
-        //                    tempPtr = tempPtr->prevNode;
-        //                }
-    }
-    template <typename DataTemplate>
-    DataTemplate DataClass<DataTemplate>::top()
-    {
-        if (!isEmpty())
-        {
-            return head->data;
-        }
-        else
-        {
-            throw (string)"nemjo";
-        }
-        return 0;
-    }
-    template <typename DataTemplate>
-    DataTemplate DataClass<DataTemplate>::pop()
-    {
-        if (isEmpty()) {
-            throw (string)"nemjo";
-        }
-        DataTemplate tempData = head->data;
-        Node* tempHead = head;
-        head = head->prevNode;
-        delete tempHead;
-        
-        return tempData;
-    }
-    template <typename DataTemplate>
-    void DataClass<DataTemplate>::push(DataTemplate value)
-    {
-        Node* tempNode = new Node;
-        tempNode->data = value;
-        tempNode->prevNode = head;
-        head = tempNode;
-    }
-
-
-
-
-    template <typename DataTemplate>
-    void DataClass<DataTemplate>::PrintOut(){}
-    template <typename DataTemplate>
-    DataTemplate DataClass<DataTemplate>::GetValue(){}
-    template <typename DataTemplate>
-    void DataClass<DataTemplate>::StepNext(){}
-    template <typename DataTemplate>
-    void DataClass<DataTemplate>::RemoveAct(){}
-    template <typename DataTemplate>
-    void DataClass<DataTemplate>::InsertLast(){}
-    template <typename DataTemplate>
-    void DataClass<DataTemplate>::InsertAfter(){}
-    template <typename DataTemplate>
-    void DataClass<DataTemplate>::RemoveFirst(){}
-    template <typename DataTemplate>
-    void DataClass<DataTemplate>::RemoveLast(){}
+template<typename DataTemplate>
+void DataClass<DataTemplate>::PrintOut()
+{
+}
+template<typename DataTemplate>
+DataTemplate DataClass<DataTemplate>::GetValue()
+{
+}
+template<typename DataTemplate>
+void DataClass<DataTemplate>::StepNext()
+{
+}
+template<typename DataTemplate>
+void DataClass<DataTemplate>::RemoveAct()
+{
+}
+template<typename DataTemplate>
+void DataClass<DataTemplate>::InsertLast()
+{
+}
+template<typename DataTemplate>
+void DataClass<DataTemplate>::InsertAfter()
+{
+}
+template<typename DataTemplate>
+void DataClass<DataTemplate>::RemoveFirst()
+{
+}
+template<typename DataTemplate>
+void DataClass<DataTemplate>::RemoveLast()
+{
+}
 
 int main()
 {
--- Data/DataClass.h
+++ Data/DataClass.h
@@ -3,48 +3,46 @@
 
 #include <iostream>
 
-template <typename DataTemplate>
+template<typename DataTemplate>
 
 //typedef int DataTemplate;
 class DataClass
 {
 public:
-    class Node {
-    public:
-        DataTemplate data;
-        Node* prevNode;
-        Node() :  prevNode(NULL)
-        {
-        }
-        Node(Node* inNode,DataTemplate inData) : data(inData), prevNode(inNode)
-        {
-        }
-    };
-    Node* head;
-    DataClass();
-    virtual ~DataClass();
+	class Node
+	{
+	public:
+		DataTemplate data;
+		Node* prevNode;
+		Node() :
+				prevNode(NULL)
+		{
+		}
+		Node(Node* inNode, DataTemplate inData) :
+				data(inData), prevNode(inNode)
+		{
+		}
+	};
+	Node* head;
+	DataClass();
+	virtual ~DataClass();
 protected:
-    virtual bool isEmpty();
-    virtual void print(std::string &outDataTemplate);
-    virtual DataTemplate top();
-    virtual DataTemplate pop();
-    virtual void push(DataTemplate value);
-    
-    
-    
-    virtual void PrintOut();
-    virtual DataTemplate GetValue();
-    virtual void StepNext();
-    virtual void RemoveAct();
-    
-    virtual void InsertLast();
-    virtual void InsertAfter();
-    virtual void RemoveFirst();
-    virtual void RemoveLast();
-    
-    
-    
-};
+	virtual bool isEmpty();
+	virtual void print(std::string &outDataTemplate);
+	virtual DataTemplate top();
+	virtual DataTemplate pop();
+	virtual void push(DataTemplate value);
 
+	virtual void PrintOut();
+	virtual DataTemplate GetValue();
+	virtual void StepNext();
+	virtual void RemoveAct();
+
+	virtual void InsertLast();
+	virtual void InsertAfter();
+	virtual void RemoveFirst();
+	virtual void RemoveLast();
+
+};
 
 #endif
